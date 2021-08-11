0e7fe62b5679259528400d40dc0f47257497ffdb
coldfear457@gmail.com <coldfear457@gmail.com>
2021-08-11 15:59:37 +0600

    feal: decision

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  var result;
+  
+    if (matrix == null){
+      return [];
+    } else { matrix.map(item => {
+      for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 == 0) {
+          matrix[i] = matrix[i].sort((a, b) => a - b);
+        } else {matrix[i] = matrix[i].sort((a, b) => b - a);}
+        result = matrix;
+      }
+    })
+    }
+  return matrix.flat();
 }
